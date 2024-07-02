9b6973a003188043b67351367ee4d50fa7cd458e
OneTen19 <khy000426@naver.com>
2024-07-02 11:00:37 +0900

    Setting home TableViewCell and it's CollectionView

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -22,6 +22,8 @@ class HomeViewController: UIViewController {
         
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
+        
+        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
     }
     
     override func viewDidLayoutSubviews() {
@@ -61,7 +63,7 @@ extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "header"
+        return "header123"
     }
     
 }
