4da82a521bbf1a3223fa008274f52adff224cf72
OneTen19 <khy000426@naver.com>
2024-07-04 11:07:29 +0900

    [feat] 주간 박스오피스 API로 변경

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -10,7 +10,12 @@ import Foundation
 
 struct Constants {
     static let API_KEY = "fd8f2df19115a92a87c4e38f38d40bfd"
-    static let itemPerPage = "&itemPerPage=20"
+    
+    // 주간 박스오피스, 영화 전체목록, 데일리 박스오피스 등등 선택해서 불러오는 곳 (일단 지금은 주간 박스오피스로)
+    static let urlString = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key="
+    
+    static let itemPerPage = "&itemPerPage=10"
+    static let targetDt = "&targetDt=20240505"
 }
 
 
@@ -20,9 +25,8 @@ class APICaller {
     
     func getTrendingMovies(completion: @escaping (String) -> Void){
         
-        let urlString = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key="
-        + Constants.API_KEY
-        + "&targetDt=20230101"
+        let urlString = Constants.urlString + Constants.API_KEY + Constants.itemPerPage + Constants.targetDt
+
         
         guard let url = URL(string: urlString) else {return}
         
@@ -33,18 +37,19 @@ class APICaller {
             do{
                 
                 // 받아온 API 값 저장
-                let result = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any]
+                let resultAPI = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any]
                 
+//                print(resultAPI!)
                 
-                // 1차 유니코드 디코딩 -> 박스오피스결과에 박스오피스 리스트가 들어있어서 이렇게 두 번에 걸쳐 디코딩해야 하는 듯??
-                let boxOfficeResult = result!["boxOfficeResult"] as? [String: Any]
+                // 1차 유니코드 디코딩 -> 원하는 영화 리스트가 영화결과라는 항목 내에 있어서 두번에 걸쳐서 디코딩해줘야하는걸로 추정
+                let boxOfficeResult = resultAPI!["boxOfficeResult"] as? [String: Any]
                 
+//                print(boxOfficeResult!)
                 
-                // 2차 유니코드 디코딩 -> 이게 리스트들 저장된 변수
-                let dailyBoxOfficeList = boxOfficeResult!["dailyBoxOfficeList"] as? [[String: Any]]
-  
+                // 2차 유니코드 디코딩 -> 이게 영화 리스트들 저장된 변수
+                let weeklyBoxOfficeList = boxOfficeResult!["weeklyBoxOfficeList"] as? [[String: Any]]
                 
-                dailyBoxOfficeList?.forEach({ item in
+                weeklyBoxOfficeList?.forEach({ item in
                     print(item)
                     print("\n")
                 })
