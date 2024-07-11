e6c3dc893a84e57484c43052a60db6ceaa430029
김한열 Hanyeol Kim <63261054+OneTen19@users.noreply.github.com>
2024-07-11 16:59:38 +0900

    [feat] Creating TitleViewModel (#30)

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by OneTen on 7/11/24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+    
+}
