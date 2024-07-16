8facc3cd44c50c16ad0066468c719f33b68ecd55
김한열 Hanyeol Kim <63261054+OneTen19@users.noreply.github.com>
2024-07-16 11:48:36 +0900

    [feat] Creating TitlePreviewViewController (#47)

---
+++ Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by OneTen on 7/15/24.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let titleName: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+    
+}
--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
+protocol CollectionViewTableViewCellDelegate: AnyObject {
+    func collectionViewTableViewCellDidTapCell(_ cell: CollectionViewTableViewCell, viewModel: TitlePreviewViewModel)
+}
+
+
 class CollectionViewTableViewCell: UITableViewCell {
 
     static let identifier = "CollectionViewTableViewCell"
+    
+    weak var delegate: CollectionViewTableViewCellDelegate?
     
     private var titles: [Title] = [Title]()
     
@@ -85,10 +92,16 @@ extension CollectionViewTableViewCell : UICollectionViewDelegate, UICollectionVi
         guard let titleName = title.original_title ?? title.name else {return}
         
         
-        APICaller.shared.getMovie(with: titleName + " trailer") { result in
+        APICaller.shared.getMovie(with: titleName + " trailer") { [weak self] result in
             switch result {
             case .success(let videoElement):
-                print(videoElement.id)
+                
+                let title = self?.titles[indexPath.row]
+                guard let titleOverview = title?.overview else {return}
+                guard let strongSelf = self else {return}
+                let viewModel = TitlePreviewViewModel(titleName: titleName, youtubeView: videoElement, titleOverview: titleOverview)
+                self?.delegate?.collectionViewTableViewCellDidTapCell(strongSelf, viewModel: viewModel)
+                
             case .failure(let error):
                 print(error.localizedDescription)
             }
